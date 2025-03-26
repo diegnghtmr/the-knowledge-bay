1908dcb5eb2e0e8b006d14b6007694bcaba4d3d9
diegnghtmr <diegoa.floresq@uqvirtual.edu.co>
2025-03-26 01:08:33 -0500

    Initialize project structure with Spring Boot and Maven configuration

---
+++ src/main/java/co/edu/uniquindio/theknowledgebay/TheKnowledgeBayApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.theknowledgebay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheKnowledgeBayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheKnowledgeBayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uniquindio/theknowledgebay/TheKnowledgeBayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.theknowledgebay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheKnowledgeBayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
