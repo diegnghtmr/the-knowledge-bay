df07940d57c6a8f8213e9144f14074f2b47fad02
Jorge-ivan-aa <jorgei.acostaa@uqvirtual.edu.co>
2025-03-28 17:00:44 -0500

    refactor: rename frontend to client
    Updated all references to the frontend folder and renamed main application classes.

--- src/main/java/co/edu/uniquindio/theknowledgebay/model/User.java
+++ src/main/java/co/edu/uniquindio/theknowledgebay/model/User.java
@@ -11,4 +11,17 @@ public class User {
     private String username;
     private String email;
     private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
+
