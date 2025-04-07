3217fbaad9520ef7895efc9db7bd80a5d2ad35fc
Jorge-ivan-aa <jorgei.acostaa@uqvirtual.edu.co>
2025-04-07 09:04:40 -0500

    chore: configure Lombok and Maven compiler plugin
    - Specified Lombok version in annotationProcessorPaths.
    - Changed Lombok scope to compileOnly and added annotationProcessor.
    - Updated maven-compiler-plugin with explicit version, source, and target.
    - Modified .gitignore to exclude .bkp files.

--- src/main/java/co/edu/uniquindio/theknowledgebay/model/User.java
+++ src/main/java/co/edu/uniquindio/theknowledgebay/model/User.java
@@ -11,17 +11,5 @@ public class User {
     private String username;
     private String email;
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
 
